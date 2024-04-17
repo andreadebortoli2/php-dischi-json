07cb3b3e942d5ed29e457c1ca191663ae3f5bbfc
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-04-17 17:15:14 +0200

    add style css

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
     <title>php-dischi-json</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <link rel="stylesheet" href="./assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 </head>
 
 <body>
@@ -18,9 +18,27 @@
 
     <div id="app">
 
-        <div v-for="album in albums">
-            {{album.title}}
-        </div>
+        <header>
+
+        </header>
+
+        <main>
+            <div class="container">
+                <div class="row">
+                    <div class="col-4 g-4" v-for="album in albums">
+                        <div class="card py-3 px-5">
+                            <img class="card-img-top w-75 mx-auto" :src="album.poster" :alt="`cover of the album ${album.title} from ${album.author}`" />
+                            <div class="card-body text-center">
+                                <h4 class="card-title">{{album.title}}</h4>
+                                <p class="card-text">{{album.author}}</p>
+                                <h5 class="card-title">{{album.year}}</h5>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </main>
 
     </div>
 
